5d53ce4080627167eefbb6fcc65949ff905a00f2
Виктория Фамутдинова < @42-2>
2024-09-25 11:47:21 +0500

    n

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -24,5 +24,49 @@ namespace WpfApp2
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int a = 1234;
+            string str = a.ToString();
+            int[] b = new int[str.Length];
+            for (int i = 0; i < str.Length; i++)
+            {
+                b[i] = str[i];
+            }
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                int l = 5;
+                int[] a = new int[l];
+                set(a, 0, 1);
+                Console.ReadKey(true);
+            }
+            static void set(int[] a, int n, int l) 
+            {
+                for (int i = 0; i < l; i++)
+                {
+                    int j;
+                    for (j = 0; j < n; j++)
+                    {
+                        if (a[j] == i + 1)
+                            break;
+                        if (j == n)
+                        {
+                            a[n] = i + 1;
+                            if (n < l - 1) set(a, n + 1, l);
+                            else
+                            {
+                             for (int k = 0; k < l; k++)
+                                    Console.Write(a[k]);
+                             Console.WriteLine();
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
